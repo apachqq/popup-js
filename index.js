d563e7b8631d7b31ce89044daefa06f941a7e470
apachqq <work.tsyganov@mail.ru>
2022-03-21 18:07:22 +0300

    'button-onclick'

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 let popup = document.querySelector('.popup')
 let openPopupButton = document.querySelector('.button-show')
 let closePopupButton = document.querySelector('.button-hide')
+
+openPopupButton.onclick = function (evt) {
+    evt.preventDefault()
+    popup.classList.add('popup--open')
+}
